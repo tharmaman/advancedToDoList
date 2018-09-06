02651ee4eabd803b1f2cc67481894bc65519535d
tharmaman <daniel.tharmathurai@mail.mcgill.ca>
2018-09-06 19:19:08 -0400

    Added todos

--- public/app.js
+++ public/app.js
@@ -2,17 +2,43 @@ $(document).ready(function(){
     // request /api/todos
     $.getJSON("/api/todos")
     .then(addTodos)
-    .catch(handleErr(err))
+    .catch(handleErr(handleErr))
+
+    $("#todoInput").keypress(function(event){
+        if (event.which === 13){
+            createTodo();
+        }
+    })
 });
 
 function addTodos(todos){
     // add todos to page here
     todos.forEach(function(todo){
-        var newTodo = $("<li class = 'task'>" + todo.name + "</li>");
-        $(".list").append(newTodo); 
+        addTodo(todo);
     });
 }
 
+function addTodo(todo){
+    var newTodo = $("<li class = 'task'>" + todo.name + "</li>");
+    if(todo.completed){
+        newTodo.addClass("done");
+    }
+    $(".list").append(newTodo); 
+}
+
 function handleErr(err){
-    res.send(err);
+    console.log(err);
+}
+
+function createTodo(){
+    var userInput = $("#todoInput").val();
+    // post to /api/todos
+    $.post('/api/todos', {name: userInput})
+    .then(function(newTodo){
+        $("#todoInput").val('');
+        addTodo(newTodo);
+    })
+    .catch(function(err){
+        console.log(err);
+    })
 }
