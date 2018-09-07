f325babf36ffb2c6946aee68ed9138cd0dffecc7
tharmaman <daniel.tharmathurai@mail.mcgill.ca>
2018-09-06 20:18:56 -0400

    Added complete toggle functionality and connected it with DB

--- public/app.js
+++ public/app.js
@@ -10,7 +10,13 @@ $(document).ready(function(){
         }
     })
 
-    $(".list").on("click", "span", function(){
+    $(".list").on("click", "li", function(){
+        updateTodo($(this));
+    })
+
+    $(".list").on("click", "span", function(event){
+        // stop li click from triggering
+        event.stopPropagation();
         var toBeRemoved = $(this).parent();
         removeTodo(toBeRemoved);
     })
@@ -26,6 +32,7 @@ function addTodos(todos){
 function addTodo(todo){
     var newTodo = $("<li class = 'task'>" + todo.name + "<span>X</span></li>");
     newTodo.data("id",todo._id);
+    newTodo.data("completed", todo.completed);
     if(todo.completed){
         newTodo.addClass("done");
     }
@@ -62,5 +69,23 @@ function removeTodo(todo){
     })
     .catch(function(err){
         console.log(err);
+    })
+}
+
+function updateTodo(todo){
+    var clickedId = todo.data("id");
+    var updateURL = "/api/todos/" + clickedId;
+    // change to opposite once clicked
+    var isDone = !todo.data("completed");
+    var updateData = {completed: isDone};
+
+    $.ajax({
+        method: "PUT",
+        url: updateURL,
+        data: updateData
+    })
+    .then(function(updatedTodo){
+        todo.toggleClass("done");
+        todo.data("completed", isDone);
     })
 }
